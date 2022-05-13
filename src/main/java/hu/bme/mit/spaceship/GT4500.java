36c50463dc1ef52605f8d509e0355bea59de5644
trombitasp <trombitasp@gmail.com>
2022-05-13 12:04:43 +0200

    labor megoldaas

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,9 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore pts, TorpedoStore sts) {
+    this.primaryTorpedoStore = pts;     //new TorpedoStore(10);
+    this.secondaryTorpedoStore = sts;   //new TorpedoStore(10);
   }
 
   public boolean fireLaser(FiringMode firingMode) {
@@ -78,7 +78,6 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
         if (! primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(primaryTorpedoStore.getTorpedoCount());
           wasPrimaryFiredLast = true;
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,18 +7,22 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
-
+  private  TorpedoStore mockTs1;
+  private  TorpedoStore mockTs2;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockTs1 = mock(TorpedoStore.class);
+    mockTs2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTs1, mockTs2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockTs1.fire(1)).thenReturn(true);
+    when(mockTs2.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
@@ -29,7 +33,11 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockTs1.getTorpedoCount()).thenReturn(1);
+    when(mockTs1.fire(1)).thenReturn(true);
 
+    when(mockTs2.getTorpedoCount()).thenReturn(2);
+    when(mockTs2.fire(2)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
